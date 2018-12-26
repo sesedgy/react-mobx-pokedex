3a9e44e0229e6efbe7f8517e57ef424b71f18125
Луканин Михаил <m_lukanin@itgroup.local>
2018-12-26 11:55:48 +0300

    Updates

---
+++ precache-manifest.f5c56bbf00388940ae6522b2cb76d83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "25926ced08d6d6640b14b46fb1b8099a",
+    "url": "/react-mobx-pokedex/static/media/Pokeball.25926ced.png"
+  },
+  {
+    "revision": "a59287b2e7b9053b69d2",
+    "url": "/react-mobx-pokedex/static/js/runtime~main.a59287b2.js"
+  },
+  {
+    "revision": "512041d27fa296905fda",
+    "url": "/react-mobx-pokedex/static/js/main.512041d2.chunk.js"
+  },
+  {
+    "revision": "feba8aa4d6191043a8fb",
+    "url": "/react-mobx-pokedex/static/js/1.feba8aa4.chunk.js"
+  },
+  {
+    "revision": "512041d27fa296905fda",
+    "url": "/react-mobx-pokedex/static/css/main.f9404750.chunk.css"
+  },
+  {
+    "revision": "6d6bc442907479cad41e24c7df11ac6b",
+    "url": "/react-mobx-pokedex/index.html"
+  }
+];
